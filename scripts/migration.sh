d4486d8398ccac1bd182ec0a42823549aaacba4f
daksh03 <dakshchoudhary.1234@gmail.com>
2026-08-07 13:46:58 +0530

    My migration

---
+++ scripts/migration.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SOURCE_BASE="https://github.com/migrationPOCAction"
+TARGET_BASE="https://github.com/migrationPOCActiontrial"
+
+repos=(
+  repo1
+)
+
+for repo in "${repos[@]}"
+do
+  echo "Migrating ${repo} ..."
+
+  git clone --mirror "$SOURCE_BASE/${repo}.git"
+
+  cd "${repo}.git"
+
+  git remote set-url origin "$TARGET_BASE/${repo}.git"
+
+  git push --mirror
+
+  cd ..
+  rm -rf "${repo}.git"
+
+  echo "$repo migrated successfully"
+done
